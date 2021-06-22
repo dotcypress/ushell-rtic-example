fc1fc00e770abf5a0850a6dad58d9c6305294012
Vitaly Domnikov <dotcypress@gmail.com>
2021-06-21 22:27:58 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,34 @@ extern crate ushell;
 use core::fmt::Write;
 use core::usize;
 
-use hal::{
-    gpio::{gpioa::PA5, *},
-    prelude::*,
-    serial, stm32,
-    timer::*,
-};
+use hal::{gpio::*, prelude::*, serial, stm32, timer::*};
 use ushell::{autocomplete::StaticAutocomplete, history::LRUHistory, Input, UShell};
 
-pub type Led = PA5<Output<PushPull>>;
-pub type BlinkyTimer = Timer<stm32::TIM16>;
+const SHELL_PROMT: &str = "#> ";
+const CR: &str = "\r\n";
+const HELP: &str = "\r\n\
+\x1b[31mL\x1b[32mE\x1b[34mD \x1b[33mBlinky Shell \x1b[0mv.0\r\n\r\n\
+USAGE:\r\n\
+\t command [arg]\r\n\r\n\
+COMMANDS:\r\n\
+\t set <Hz>\t Set animation frequency in Hertz [1-100]\r\n\
+\t status\t\t Get animation status\r\n\
+\t on\t\t Start animation\r\n\
+\t off\t\t Stop animation\r\n\
+\t clear\t\t Clear screen\r\n\
+\t help\t\t Print this message\r\n\r\n";
+
 pub type Serial = serial::Serial<stm32::USART2, serial::FullConfig>;
+pub type BlinkTimer = Timer<stm32::TIM16>;
+pub type Led = gpioa::PA5<Output<PushPull>>;
 
 #[rtic::app(device = hal::stm32, peripherals = true)]
 const APP: () = {
     struct Resources {
         led: Led,
-        status: bool,
-        blinky_freq: u8,
-        blinky_timer: BlinkyTimer,
+        blink_freq: u8,
+        blink_enabled: bool,
+        blink_timer: BlinkTimer,
         shell: UShell<Serial, StaticAutocomplete<6>, LRUHistory<32, 4>, 32>,
     }
 
@@ -39,9 +48,9 @@ const APP: () = {
         let mut rcc = ctx.device.RCC.constrain();
         let port_a = ctx.device.GPIOA.split(&mut rcc);
 
-        let mut blinky_timer = ctx.device.TIM16.timer(&mut rcc);
-        blinky_timer.start(4.hz());
-        blinky_timer.listen();
+        let mut blink_timer = ctx.device.TIM16.timer(&mut rcc);
+        blink_timer.start(4.hz());
+        blink_timer.listen();
 
         let mut serial = ctx
             .device
@@ -56,50 +65,42 @@ const APP: () = {
         serial.listen(serial::Event::Rxne);
 
         let autocomplete = StaticAutocomplete(["clear", "help", "off", "on", "set ", "status"]);
-        let shell = UShell::new(serial, autocomplete, LRUHistory::default());
+        let history = LRUHistory::default();
+        let shell = UShell::new(serial, autocomplete, history);
 
         let led = port_a.pa5.into_push_pull_output();
         init::LateResources {
             led,
             shell,
-            blinky_timer,
-            blinky_freq: 2,
-            status: false,
+            blink_timer,
+            blink_freq: 2,
+            blink_enabled: false,
         }
     }
 
-    #[task(binds = TIM16, priority = 2, resources = [blinky_timer, status, led])]
-    fn blinky_timer_tick(ctx: blinky_timer_tick::Context) {
-        if *ctx.resources.status {
+    #[task(binds = TIM16, priority = 2, resources = [blink_timer, blink_enabled, led])]
+    fn blink_timer_tick(ctx: blink_timer_tick::Context) {
+        if *ctx.resources.blink_enabled {
             ctx.resources.led.toggle().expect("Failed to blink o_0");
+        } else {
+            ctx.resources
+                .led
+                .set_low()
+                .expect("Failed to switch led off");
         }
-        ctx.resources.blinky_timer.clear_irq();
+        ctx.resources.blink_timer.clear_irq();
     }
 
-    #[task(binds = USART2, priority = 1, resources = [shell, blinky_timer, blinky_freq, status])]
+    #[task(binds = USART2, priority = 1, resources = [shell, blink_timer, blink_freq, blink_enabled])]
     fn serial_data(ctx: serial_data::Context) {
-        const HELP: &str = "\r\n\
-            \x1b[31mL\x1b[32mE\x1b[34mD \x1b[33mBlinky Shell \x1b[0mv.0\r\n\r\n\
-            USAGE:\r\n\
-            \t command [arg]\r\n\r\n\
-            COMMANDS:\r\n\
-            \t set <hz>\t Set animation frequency [1-100]\r\n\
-            \t status\t\t Get animation status\r\n\
-            \t on\t\t Start animation\r\n\
-            \t off\t\t Stop animation\r\n\
-            \t clear\t\t Clear screen\r\n\
-            \t help\t\t Print this message\r\n\r\n";
-        const CR: &str = "\r\n";
-        const SHELL_PROMT: &str = "~> ";
-
         let shell = ctx.resources.shell;
-        let blinky_freq = ctx.resources.blinky_freq;
-        let mut blinky_timer = ctx.resources.blinky_timer;
-        let mut status = ctx.resources.status;
+        let blink_freq = ctx.resources.blink_freq;
+
+        let mut blink_timer = ctx.resources.blink_timer;
+        let mut blink_enabled = ctx.resources.blink_enabled;
 
         loop {
             match shell.poll() {
-                Ok(None) => break,
                 Ok(Some(Input::Command(_, cmd))) => {
                     match cmd {
                         "" => {
@@ -112,44 +113,45 @@ const APP: () = {
                             shell.clear().ok();
                         }
                         "on" => {
-                            status.lock(|s| *s = true);
+                            blink_enabled.lock(|e| *e = true);
                             shell.write_str(CR).ok();
                         }
                         "off" => {
-                            status.lock(|s| *s = false);
+                            blink_enabled.lock(|e| *e = false);
                             shell.write_str(CR).ok();
                         }
                         "status" => {
-                            let on = status.lock(|s| *s);
-                            let status = if on { "enabled" } else { "disabled" };
+                            let on = blink_enabled.lock(|e| *e);
+                            let status = if on { "On" } else { "Off" };
                             write!(
                                 shell,
-                                "{0:}animation: {1:}{0:}frequency: {2:}Hz{0:}",
-                                CR, status, blinky_freq
+                                "{0:}Animation: {1:}{0:}Frequency: {2:}Hz{0:}",
+                                CR, status, blink_freq
                             )
                             .ok();
                         }
-                        _ if cmd.len() > 4 && cmd.starts_with("set ") => {
-                            let (_, arg) = cmd.split_at(4);
-                            match btoi::btoi(arg.as_bytes()) {
+                        _ if cmd.starts_with("set ") => {
+                            let (_, freq) = cmd.split_at(4);
+                            match btoi::btoi(freq.as_bytes()) {
                                 Ok(freq) if freq > 0 && freq <= 100 => {
-                                    *blinky_freq = freq;
-                                    blinky_timer.lock(|t| {
+                                    *blink_freq = freq;
+                                    blink_timer.lock(|t| {
                                         t.start((freq as u32 * 2).hz());
                                     });
                                     shell.write_str(CR).ok();
                                 }
                                 _ => {
-                                    write!(shell, "{0:}invalid frequency{0:}", CR).ok();
+                                    write!(shell, "{0:}unsupported frequency{0:}", CR).ok();
                                 }
                             }
                         }
                         _ => {
-                            write!(shell, "{0:}invalid command{0:}", CR).ok();
+                            write!(shell, "{0:}unknown command{0:}", CR).ok();
                         }
                     }
                     shell.write_str(SHELL_PROMT).ok();
                 }
+                Ok(None) => break,
                 _ => {}
             }
         }
